f2595ce0dd6357aabecd1c77209c14c726f2a5bc
Amir Bitaraf <bitaraf.amir@gmail.com>
2015-06-11 22:10:20 +0430

    Single IP X_FORWARDED_FOR

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ if os.environ.get("DEVELOPMENT"):
 
 def get_client_ip(request):
     if request.headers.getlist("X-Forwarded-For"):
-        ip = request.headers.getlist("X-Forwarded-For")[0]
+        ip = request.headers.getlist("X-Forwarded-For")[0].split(',')[0]
     else:
         ip = request.remote_addr
     return ip
